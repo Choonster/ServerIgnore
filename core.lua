e14fc6e7a0f057bfe95686757ad5e20602b10261
Choonster <Choonster.2010@gmail.com>
2013-11-24 16:06:59 +1100

    Update for 5.4
    - Add `SERVERIGNORE_ENABLED` to TOC SavedVariables tag (allowing the
    enabled state to be saved correctly)
    - Replace individual event listings in `Add/RemoveFilters` functions
    with single table of events to filter
    - Add `printf` function for slash command
    - Improve detection of own server in `add` and `remove` commands
    - Replace manual concatenation of server names in `list` command with a
    temporary array and `table.concat`
    - Rewrite ADDON_LOADED code to set `SERVERIGNORE_ENABLED` default using
    an explicit `if` instead of `and`/`or`.

--- core.lua
+++ core.lua
@@ -1,6 +1,28 @@
 ﻿local addon, ns = ...
 
-local units = {
+-- Chat events (without the CHAT_MSG_ prefix) to filter
+local Events = {
+	"ACHIEVEMENT",
+	"AFK",
+	"BATTLEGROUND",
+	"BATTLEGROUND_LEADER",
+	"CHANNEL",
+	"DND",
+	"EMOTE",
+	"PARTY",
+	"PARTY_LEADER",
+	"RAID",
+	"RAID_LEADER",
+	"RAID_WARNING",
+	"SAY",
+	"TEXT_EMOTE",
+	"WHISPER",
+	"YELL",
+}
+
+-- Valid unitIDs
+local Units = {
+	["player"] = true, -- It's entirely possible someone will try to ignore their own server
 	["target"] = true,
 	["focus"] = true,
 	["mouseover"] = true
@@ -8,141 +30,118 @@ local units = {
 
 for i = 1, 40 do
 	if i <= 4 then
-		units["party" .. i] = true
+		Units["party" .. i] = true
 	end
-	
+
 	if i <= 5 then
-		units["arena" .. i] = true
+		Units["arena" .. i] = true
 	end
-	
-	units["raid" .. i] = true
+
+	Units["raid" .. i] = true
 end
 
-local db;
+local DB;
 
-local function filterFunc(self, event, msg, author, ...)
+local function FilterFunc(chatFrame, event, msg, author, ...)
 	local name, realm = author:lower():match("([^-]+)[-]?([^-]*)")
-	
-	if db[realm] then
+
+	if DB[realm] then
 		return true
 	end
 end
 
 local function AddFilters()
-	ChatFrame_AddMessageEventFilter("CHAT_MSG_ACHIEVEMENT", filterFunc)
-	ChatFrame_AddMessageEventFilter("CHAT_MSG_AFK", filterFunc)
-	ChatFrame_AddMessageEventFilter("CHAT_MSG_BATTLEGROUND", filterFunc)
-	ChatFrame_AddMessageEventFilter("CHAT_MSG_BATTLEGROUND_LEADER", filterFunc)
-	ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", filterFunc)
-	ChatFrame_AddMessageEventFilter("CHAT_MSG_DND", filterFunc)
-	ChatFrame_AddMessageEventFilter("CHAT_MSG_EMOTE", filterFunc)
-	ChatFrame_AddMessageEventFilter("CHAT_MSG_PARTY", filterFunc)
-	ChatFrame_AddMessageEventFilter("CHAT_MSG_PARTY_LEADER", filterFunc)
-	ChatFrame_AddMessageEventFilter("CHAT_MSG_RAID", filterFunc)
-	ChatFrame_AddMessageEventFilter("CHAT_MSG_RAID_LEADER", filterFunc)
-	ChatFrame_AddMessageEventFilter("CHAT_MSG_RAID_WARNING", filterFunc)
-	ChatFrame_AddMessageEventFilter("CHAT_MSG_SAY", filterFunc)
-	ChatFrame_AddMessageEventFilter("CHAT_MSG_TEXT_EMOTE", filterFunc)
-	ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER", filterFunc)
-	ChatFrame_AddMessageEventFilter("CHAT_MSG_YELL", filterFunc)
+	for i = 1, #Events do
+		ChatFrame_AddMessageEventFilter("CHAT_MSG_" .. Events[i], FilterFunc)
+	end
 end
 
 local function RemoveFilters()
-	ChatFrame_RemoveMessageEventFilter("CHAT_MSG_ACHIEVEMENT", filterFunc)
-	ChatFrame_RemoveMessageEventFilter("CHAT_MSG_AFK", filterFunc)
-	ChatFrame_RemoveMessageEventFilter("CHAT_MSG_BATTLEGROUND", filterFunc)
-	ChatFrame_RemoveMessageEventFilter("CHAT_MSG_BATTLEGROUND_LEADER", filterFunc)
-	ChatFrame_RemoveMessageEventFilter("CHAT_MSG_CHANNEL", filterFunc)
-	ChatFrame_RemoveMessageEventFilter("CHAT_MSG_DND", filterFunc)
-	ChatFrame_RemoveMessageEventFilter("CHAT_MSG_EMOTE", filterFunc)
-	ChatFrame_RemoveMessageEventFilter("CHAT_MSG_PARTY", filterFunc)
-	ChatFrame_RemoveMessageEventFilter("CHAT_MSG_PARTY_LEADER", filterFunc)
-	ChatFrame_RemoveMessageEventFilter("CHAT_MSG_RAID", filterFunc)
-	ChatFrame_RemoveMessageEventFilter("CHAT_MSG_RAID_LEADER", filterFunc)
-	ChatFrame_RemoveMessageEventFilter("CHAT_MSG_RAID_WARNING", filterFunc)
-	ChatFrame_RemoveMessageEventFilter("CHAT_MSG_SAY", filterFunc)
-	ChatFrame_RemoveMessageEventFilter("CHAT_MSG_TEXT_EMOTE", filterFunc)
-	ChatFrame_RemoveMessageEventFilter("CHAT_MSG_WHISPER", filterFunc)
-	ChatFrame_RemoveMessageEventFilter("CHAT_MSG_YELL", filterFunc)
+	for i = 1, #Events do
+		ChatFrame_RemoveMessageEventFilter("CHAT_MSG_" .. Events[i], FilterFunc)
+	end
 end
 
 SLASH_SERVERIGNORE1, SLASH_SERVERIGNORE2 = "/serverignore", "/signore"
 
-local function q(s)
-	return ("%q"):format(tostring(s))
-end
+do
+	local function printf(formatString, ...)
+		print("|cFF33ff99ServerIgnore:|r", formatString:format(...))
+	end
 
-local prefix = "|cff33ff99ServerIgnore:|r"	
+	local serverListTemp = {}
 
-SlashCmdList.SERVERIGNORE = function(input)
-	local cmd, name = input:lower():trim():match("^(%S+)%s*(.*)$")
-	
-	if cmd == "toggle" then
-		SERVERIGNORE_ENABLED = not SERVERIGNORE_ENABLED
-		
-		if SERVERIGNORE_ENABLED then
-			print(prefix, "|cff00ff00Enabled|r")
-			AddFilters()
+	local playerServer = GetRealmName():lower()
+
+	SlashCmdList.SERVERIGNORE = function(input)
+		local cmd, name = input:lower():trim():match("^(%S+)%s*(.*)$")
+
+		if cmd == "toggle" then
+			SERVERIGNORE_ENABLED = not SERVERIGNORE_ENABLED
+
+			if SERVERIGNORE_ENABLED then
+				printf("|cFF00ff00Enabled|r")
+				AddFilters()
+			else
+				printf("|cFFff0000Disabled|r")
+				RemoveFilters()
+			end
+		elseif cmd == "reset" then
+			wipe(DB)
+			printf("Ignore list reset.")
+		elseif cmd == "add" and name and name ~= "" then
+			local serverName, _;
+			if Units[name] then
+				_, serverName = UnitName(name)
+			else
+				serverName = name
+			end
+
+			if not serverName or serverName == playerServer then
+				printf("You cannot ignore your own server.")
+			elseif DB[serverName] then
+				printf("Server %s is already on your ignore list.", serverName)
+			else
+				DB[serverName] = true
+				printf("Server %s added to your ignore list.", serverName)
+			end
+		elseif cmd == "remove" and name and name ~= "" then
+			local serverName, _;
+			if Units[name] then
+				_, serverName = UnitName(name)
+			else
+				serverName = name
+			end
+
+			if not serverName or serverName == playerServer then
+				printf("You cannot ignore your own server.")
+			elseif DB[serverName] then
+				DB[serverName] = nil
+				printf("Server %s removed from your ignore list.", serverName)
+			else
+				printf("Server %s is not on your ignore list.", serverName)
+			end
+		elseif cmd == "list" then
+			local count = 0
+			for server, _ in pairs(DB) do
+				count = count + 1
+				serverListTemp[count] = server
+			end
+
+			if count == 0 then
+				printf("You have not ignored any servers yet.")
+			else
+				printf("Ignored Servers: %s", table.concat(serverListTemp, ", ", 1, count))
+			end
 		else
-			print(prefix, "|cff0000Disabled|r")
-			RemoveFilters()
+			printf("Slash Command Usage")
+			print("|cFFff0000/serveringore|r or |cffff0000/signore cmd server||unit")
+			print("    |cFFff0000toggle|r -- Enable/disable the AddOn.")
+			print("    |cFFff0000reset|r -- Resets your ignore list.")
+			print("    |cFFff0000add server||unit|r -- Adds a sever to your ignore list. You can either specify a server by name or use a unitID to ingore that unit's server. Valid unitIDs are target, focus, mouseover partyN, raidN and arenaN (where N is some number). You cannot ignore your own server.")
+			print("    |cFFff0000remove server||unit|r -- Removes a sever from your ignore list.")
+			print("    |cFFff0000list|r -- List the servers you have on ignore.")
 		end
-	elseif cmd == "reset" then
-		wipe(db)
-		print(prefix, "Ignore list reset.")
-	elseif cmd == "add" and name and name ~= "" then
-		local serverName, _;
-		if units[name] then
-			_, serverName = UnitName(name)
-		else
-			serverName = name
-		end
-		
-		if db[serverName] then
-			print(prefix, ("Server %s is already on your ignore list."):format(serverName))
-		elseif serverName then
-			db[serverName] = true
-			print(prefix, ("Server %s added to your ignore list."):format(serverName))
-		else
-			print(prefix, ("Unit %q is on your server."):format(name))
-		end
-	elseif cmd == "remove" and name and name ~= "" then
-		local serverName, _;
-		if units[name] then
-			_, serverName = UnitName(name)
-		else
-			serverName = name
-		end
-		
-		if db[serverName] then
-			db[serverName] = nil
-			print(prefix, ("Server %s removed from your ignore list."):format(serverName))
-		elseif serverName then
-			print(prefix, ("Server %s is not on your ignore list."):format(serverName))
-		else
-			print(preifx, ("Unit %s is on your server."):format(name))
-		end
-	elseif cmd == "list" then
-		local str = next(db)
-		local empty = true
-		local count = 0
-		
-		for server in next, db, str do
-			empty = false
-			count = (i % 3) + 1
-			str = ("%s, %s"):format(str, server)
-		end
-		print(prefix, "Ignored Servers:")
-		print(empty and "You have not ignored any servers yet." or str)
-	else
-		print(prefix, "Slash Command Usage")
-		print("|cffff0000/serveringore|r or |cffff0000/signore cmd server||unit")
-		print("    |cffff0000toggle|r -- Enable/disable the AddOn.")
-		print("    |cffff0000reset|r -- Resets your ignore list.")
-		print("    |cffff0000add server||unit|r -- Adds a sever to your ignore list. You can either specify a server by name or use a unitID to ingore that unit's server. Valid unitIDs are target, focus, mouseover partyN, raidN and arenaN (where N is some number).")
-		print("        You cannot add your own server to the list using a unitID (but you can by specifying its name). Messages from your own server will never be ignored (even if it's on the list).")
-		print("    |cffff0000remove server||unit|r -- Removes a sever to your ignore list.")
-		print("    |cffff0000list|r -- List the servers you have on ignore.")		
 	end
 end
 
@@ -150,15 +149,17 @@ local f = CreateFrame("Frame")
 f:RegisterEvent("ADDON_LOADED")
 f:SetScript("OnEvent", function(self, event, name)
 	if name == addon then
-		SERVERIGNORE_ENABLED = SERVERIGNORE_ENABLED == nil and true or SERVERIGNORE_ENABLED
-		
+		if SERVERIGNORE_ENABLED == nil then
+			SERVERIGNORE_ENABLED = true
+		end
+
 		if SERVERIGNORE_ENABLED then
 			AddFilters()
 		end
-		
+
 		SERVERIGNORE_DB = SERVERIGNORE_DB or {}
-		db = SERVERIGNORE_DB
-		
+		DB = SERVERIGNORE_DB
+
 		self:UnregisterEvent("ADDON_LOADED")
 	end
 end)
